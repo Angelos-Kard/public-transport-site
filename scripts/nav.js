ae53024554b2f2b648b1f1205fe9f6a8e0f23bf4
Angelos Kard <angelkard77@gmail.com>
2021-04-17 19:40:48 +0300

    New items in tickets
    - Added two tables in tickets.html
    - Changed links of dropdown menu
    - Fixed a bug in the dropdown menu: out of place when scrolled

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,12 +5,19 @@ let scrollPosition = 0;
 
 window.addEventListener("scroll", function () {
 
-    if( window.pageYOffset >= navbar.offsetTop ) { 
+    if( window.pageYOffset >= navbar.offsetTop ) 
+    { 
         navbar.className = "show"; 
+        document.querySelector(".dropdown-content").style.top = "50px";
+
     }
     else {
         if ( navbar.classList.contains("show") )
+        {
             navbar.classList.remove("show");
+            document.querySelector(".dropdown-content").style.top = "188px";
+        }
+            
     }
     scrollPosition = (document.body.getBoundingClientRect()).top;
 });
