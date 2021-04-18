1175b5c9fddc964220be4adacaf897701393f5d3
Angelos Kard <angelkard77@gmail.com>
2021-04-19 01:34:15 +0300

    Tckets.js

---
+++ scripts/tickets.js
@@ -0,0 +1,7 @@
+let data = '['+
+'{"id":1, "typos": "meiomeno", "timi": 0.60, "zoni":"A"},' +
+'{ "id":2, "typos": "kanoniko", "timi": 1.10, "zoni":"A"},' +
+'{ "id":3, "typos": "meiomeno", "timi": 0.80, "zoni":"B" },' +
+'{ "id":4, "typos": "kanoniko", "timi": 1.50, "zoni":"B" } ]';
+
+const mydata = JSON.parse(data);
