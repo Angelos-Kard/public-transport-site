4e0a827b9ddf46d356e824d5eb26c30de89bccac
Angelos Kard <angelkard77@gmail.com>
2021-06-04 15:22:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const port = process.env.PORT || '3000';
 const server = app.listen(port, () => {
 	console.log("Listening to port " + port)
 });
+
+process.on('SIGINT', function() {
+	console.log( "\nShutting down server (Ctrl+C)..." );
+	// some other closing procedures go here
+	process.exit(1);
+  });
