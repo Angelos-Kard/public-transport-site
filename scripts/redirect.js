5aa61fc4500ff44a1b4532573b95b2c64a6b9e2b
Angelos Kard <angelkard77@gmail.com>
2021-06-04 16:49:47 +0300

    Fixed redirection link

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,3 +1,3 @@
 const title0 = document.querySelector("#title")
 title0.style.cursor = "pointer";
-title0.onclick = () => { location.href = "index.html"};
+title0.onclick = () => { location.href = "/index"};
