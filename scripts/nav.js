ec62c9d07d36f4c6c0a1c764b6a876bac82084cd
ChrisKaldis <rtfshing@gmail.com>
2021-06-05 18:43:09 +0300

    styling the menu button

--- scripts/nav.js
+++ scripts/nav.js
@@ -9,7 +9,10 @@ scrollToTop.textContent = "↑";
 scrollToTop.addEventListener("click", function (){
     document.documentElement.scrollTo({top: 0 , behavior: "smooth"});
 });
-document.querySelector('h1').appendChild(scrollToTop);
+document.querySelector('main').appendChild(scrollToTop);
+
+const hamburger = document.querySelector(".hamburger");
+const togglerItem = document.querySelector(".toggler");
 
 window.addEventListener("scroll", function () {
 
@@ -17,6 +20,14 @@ window.addEventListener("scroll", function () {
     {
         navbar.className = "show"; 
         document.querySelector(".dropdown-content").style.top = "50px";
+
+        hamburger.style.width = "40px";
+        hamburger.style.height = "40px";
+        hamburger.style.padding = "15px";
+
+        togglerItem.style.width = "65px";
+        togglerItem.style.height = "65px";
+        
     }
     else if ( navbar.classList.contains("show") )
     {
@@ -27,4 +38,15 @@ window.addEventListener("scroll", function () {
     ( scrollPosition < -200 ) ? scrollToTop.style.display = "block" : scrollToTop.style.display = "none";
 
     scrollPosition = (document.body.getBoundingClientRect()).top;
+
+    if ( scrollPosition >= -1 )
+    {
+        hamburger.style.width = "57px";
+        hamburger.style.height = "67px";
+        hamburger.style.padding = "35px";
+
+        togglerItem.style.width = "125px";
+        togglerItem.style.height = "130px";
+    }
+
 });
